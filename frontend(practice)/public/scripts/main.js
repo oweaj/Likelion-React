05df290e6348085d3d658e95122ced4c7b31e6ba
oweaj <qwe1520156@naver.com>
2023-02-15 16:59:18 +0900

    버튼 활성 및 토글 기능 적용

--- frontend(practice)/public/scripts/main.js
+++ frontend(practice)/public/scripts/main.js
@@ -1,3 +1,3 @@
-import App from "./components/App.js";
+import App from "./App.js";
 var reactDomRoot = ReactDOM.createRoot(document.getElementById("root"));
 reactDomRoot.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(App, null)));
--- frontend(practice)/src/main.js
+++ frontend(practice)/src/main.js
@@ -1,4 +1,4 @@
-import App from "./components/App.js";
+import App from "./App.js";
 
 const reactDomRoot = ReactDOM.createRoot(document.getElementById("root"));
 
