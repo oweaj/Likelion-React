dcd8652ca1aba615dc9f389f9e2f5408e73bf572
oweaj <qwe1520156@naver.com>
2023-02-24 09:49:22 +0900

    ESLint 구성

---
+++ stateful-component-form/.eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  rules: {
+    "no-unused-vars": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/anchor-has-content": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+  },
+};
